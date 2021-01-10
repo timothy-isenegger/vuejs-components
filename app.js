fdeaba062a471c15a452da1487d5c11c8544b4e6
Timothy Isenegger <timothy.isenegger@gmail.com>
2021-01-10 12:18:52 +0100

    finish chapter one

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-Vue.component('plan', {
+let PlanComponent =  {
 	template: `
-		<div class="plan">
+		<div class="plan" v-on:click="select" :class="{'plan-active': isSelected}">
 			<div class="description">
 				<span class="title">{{name}}</span>
 			</div>
@@ -10,12 +10,46 @@ Vue.component('plan', {
 			type: String,
 			required: true
 		},
+		selectedPlan: {
+			type: String
+		}
 	},
-})
+	computed: {
+		isSelected() {
+			return this.name === this.selectedPlan;
+		}
+	},
+	methods: {
+		select() {
+			this.$emit('select', this.name);
+		}
+	}
+};
+
+let PlanPickerComponent = {
+	template: `
+		<div class="plans">
+			<plan v-for="plan in plans" :name="plan" @select="selectPlan" :selected-plan="selectedPlan"></plan>
+		</div>`,
+	components: {
+		plan: PlanComponent,
+	},
+	data () {
+		return {
+			plans: ['The Single', 'The Curious', 'The Addict'],
+			selectedPlan: null,
+		}
+	},
+	methods: {
+		selectPlan(plan) {
+			this.selectedPlan = plan;
+		}
+	}
+}
 
 new Vue({
 	el: '#app',
-	data: {
-		plans: ['The Single', 'The Curious', 'The Addict'],
+	components: {
+		'plan-picker': PlanPickerComponent,
 	}
-})
+});
